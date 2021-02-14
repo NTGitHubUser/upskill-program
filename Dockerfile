5d43a48bd5f50dd48ce641b52a941428499e8108
Nadiia Teslenko <nadiia.teslenko@gmail.com>
2021-02-14 14:41:49 +0200

    Up-skill Programm (Module 1): Create SpringBoot app for healthcheck and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:6.8.2-jdk11 AS build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build --no-daemon 
+
+
+FROM openjdk:11
+ARG JAR_FILE=build/libs/\*.jar
+COPY --from=build /home/gradle/src/build/libs/*.jar upskillproject.jar
+ENTRYPOINT ["java","-jar","/upskillproject.jar"]
---
+++ src/main/java/com/upskill/cloudinaction/HealthCheckController.java
@@ -0,0 +1,38 @@
+package com.upskill.cloudinaction;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+@RestController
+public class HealthCheckController
+{
+	@RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
+	public String healthCheck()
+	{
+		try
+		{
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(new URI("https://www.google.com/"))
+					.GET()
+					.build();
+
+			HttpResponse<String> response = HttpClient.newBuilder()
+					.build()
+					.send(request, HttpResponse.BodyHandlers.ofString());
+			return "Service Google, status: " + response.statusCode();
+		}
+		catch (URISyntaxException | IOException | InterruptedException ex)
+		{
+			return "Health check configured incorrectly: " + ex;
+		}
+	}
+}
