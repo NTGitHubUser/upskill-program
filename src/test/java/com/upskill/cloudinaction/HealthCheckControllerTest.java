59a0c692fcfeff72b07fee20a2159228d6e0696c
Nadiia Teslenko <nadiia.teslenko@gmail.com>
2021-02-28 02:02:06 +0200

    Test build wih failed Unit test

---
+++ src/test/java/com/upskill/cloudinaction/HealthCheckControllerTest.java
@@ -0,0 +1,17 @@
+package com.upskill.cloudinaction;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class HealthCheckControllerTest
+{
+
+	@Test
+	public void healthCheck()
+	{
+		Assertions.assertTrue(false);
+	}
+}
